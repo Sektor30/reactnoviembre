135b7828f9bc3921ff63e7ad4da7d09022c2c3ae
Sektor30 <sektor9130@gmail.com>
2024-12-03 12:46:26 -0500

    laptop changes 4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+
+
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+import Section from './components/Section';
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <main>
+        <Section id="home" title="Home">
+          <p>Bienvenido a nuestra página de inicio.</p>
+        </Section>
+        <Section id="about" title="About">
+          <p>Información sobre nosotros.</p>
+        </Section>
+        <Section id="services" title="Services">
+          <p>Detalles de nuestros servicios.</p>
+        </Section>
+        <Section id="contact" title="Contact">
+          <p>Información de contacto.</p>
+        </Section>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
+
+
+/* first example */
+/* import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> probando reactjs.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>bienllegados a mi Landing  </h1>
+         </header>
+        
+  
     </div>
   );
 }
 
 export default App;
+ */
